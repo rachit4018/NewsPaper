454e2a9ea34b3dfa9589ea5392539730b05c8608
rachit4018 <rachitpandya2509@gmail.com>
2024-07-11 13:15:38 -0400

    used env variables for db creds

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'newspaper_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'articles',
-        'USER': 'rachit',
-        'PASSWORD': 'rachit',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
