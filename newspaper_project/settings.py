f2380e3a554600f3beba4e396e1fef84031f99a3
rachit4018 <rachitpandya2509@gmail.com>
2024-07-11 13:09:56 -0400

    db changed to postgres

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'newspaper_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'articles',
+        'USER': 'rachit',
+        'PASSWORD': 'rachit',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
